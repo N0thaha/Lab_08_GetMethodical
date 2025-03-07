4937aec536ef9683d6a400488ed5ccc48b858674
Ethan <ethanxxjohnson@gmail.com>
2025-03-07 10:42:22 -0500

    PrettyHeader final commit

---
+++ src/PrettyHeader.java
@@ -0,0 +1,8 @@
+public class PrettyHeader {
+    public static void main(String[] args) {
+        SafeInput.prettyHeader("Welcome to Java!");
+        SafeInput.prettyHeader("Centered Message");
+        SafeInput.prettyHeader("Short");
+        SafeInput.prettyHeader("This is a longer message to test the centering.");
+    }
+}

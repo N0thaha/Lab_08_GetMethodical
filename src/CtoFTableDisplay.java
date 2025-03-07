6b5551bba1217275ea263a39bb0e850d26c5b0e1
Ethan <ethanxxjohnson@gmail.com>
2025-03-07 10:45:50 -0500

    CtoFTableDisplay final commit

---
+++ src/CtoFTableDisplay.java
@@ -0,0 +1,15 @@
+public class CtoFTableDisplay {
+    public static void main(String[] args) {
+        System.out.println("Celsius to Fahrenheit Conversion Table");
+        System.out.println("--------------------------------------");
+        System.out.printf("%-10s %-10s%n", "Celsius", "Fahrenheit");
+
+        for (int celsius = -100; celsius <= 100; celsius++) {
+            System.out.printf("%-10d %-10.2f%n", celsius, CtoF(celsius));
+        }
+    }
+
+    public static double CtoF(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+}
